615452ad39757d4428747df4d1d173adc8649b89
nemanja <pluzarev.nemanja@gmail.com>
2023-12-25 19:45:06 +0100

    Color scheme changed

--- app/src/main/java/com/example/rentproject/presentation/main/MainScreen.kt
+++ app/src/main/java/com/example/rentproject/presentation/main/MainScreen.kt
@@ -1,14 +1,19 @@
 package com.example.rentproject.presentation.main
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Menu
@@ -28,9 +33,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.rentproject.R
 import com.example.rentproject.presentation.util.RoomTabs
 import kotlinx.coroutines.launch
+import kotlin.math.floor
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
@@ -86,8 +98,7 @@ fun MainScreen() {
                         icon = {
                             Icon(
                                 imageVector = if (selectedTabIndex.value == index)
-                                    currentTab.selectedIcon else currentTab.unselectedIcon
-                                    ,
+                                    currentTab.selectedIcon else currentTab.unselectedIcon,
                                 contentDescription = "Tab Icon"
                             )
                         }
@@ -100,14 +111,54 @@ fun MainScreen() {
                 modifier = Modifier
                     .fillMaxWidth()
                     .weight(1f)
-            ) {
+            ) { page ->
                 Box(
                     modifier = Modifier.fillMaxSize(),
                     contentAlignment = Alignment.Center
                 ) {
-                    Text(text = RoomTabs.entries[selectedTabIndex.value].text)
+                    FloorPage(floorName = RoomTabs.entries[selectedTabIndex.value].text)
                 }
             }
+        }
+    }
+}
+
+
+@Composable
+fun FloorPage(
+    floorName: String
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp),
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .clip(RoundedCornerShape(20.dp))
+                    .background(MaterialTheme.colorScheme.onBackground),
+                contentAlignment = Alignment.Center
+            ) {
+                AsyncImage(
+                    model = R.mipmap.ic_launcher,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.clip(RoundedCornerShape(20.dp))
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "$floorName plan")
+            Spacer(modifier = Modifier.height(8.dp))
+
+        }
+        Column(horizontalAlignment = Alignment.Start) {
+            Text(text = "All rooms : ")
+
         }
     }
 }
--- app/src/main/java/com/example/rentproject/ui/theme/Theme.kt
+++ app/src/main/java/com/example/rentproject/ui/theme/Theme.kt
@@ -16,21 +16,53 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = yellow,
-    primaryContainer = lightGrey,
-    secondary = lightGrey,
-    background = darkGrey,
-    surface = darkGrey,
-    onPrimary = white
+    primary = yellow80,
+    onPrimary = yellow20,
+    primaryContainer = yellow30,
+    onPrimaryContainer = yellow90,
+    inversePrimary = yellow40,
+    secondary = DarkYellow80,
+    onSecondary = DarkYellow20,
+    secondaryContainer = DarkYellow30,
+    onSecondaryContainer = DarkYellow90,
+    error = Red80,
+    onError = Red20,
+    errorContainer = Red30,
+    onErrorContainer = Red90,
+    background = Grey10,
+    onBackground = Grey90,
+    surface = YellowGrey30,
+    onSurface = YellowGrey80,
+    inverseSurface = Grey90,
+    inverseOnSurface = Grey10,
+    surfaceVariant = YellowGrey30,
+    onSurfaceVariant = YellowGrey80,
+    outline = YellowGrey80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = lightGrey,
-    primaryContainer = lightGrey,
-    secondary = yellow,
-    background = white,
-    surface = white,
-    onPrimary = darkGrey,
+    primary = yellow40,
+    onPrimary = white,
+    primaryContainer = yellow90,
+    onPrimaryContainer = yellow10,
+    inversePrimary = yellow80,
+    secondary = DarkYellow40,
+    onSecondary = white,
+    secondaryContainer = DarkYellow90,
+    onSecondaryContainer = DarkYellow10,
+    error = Red40,
+    onError = white,
+    errorContainer = Red90,
+    onErrorContainer = Red10,
+    background = Grey99,
+    onBackground = Grey10,
+    surface = YellowGrey90,
+    onSurface = YellowGrey30,
+    inverseSurface = Grey20,
+    inverseOnSurface = Grey95,
+    surfaceVariant = YellowGrey90,
+    onSurfaceVariant = YellowGrey30,
+    outline = YellowGrey50
 )
 
 @Composable
