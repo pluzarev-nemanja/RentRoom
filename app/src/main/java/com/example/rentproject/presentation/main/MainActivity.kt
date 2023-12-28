705039fb0fe9c25bf7722da56857901cc56cbff9
nemanja <pluzarev.nemanja@gmail.com>
2023-12-28 10:10:06 +0100

    Splash screen implemented

--- app/src/main/java/com/example/rentproject/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/rentproject/presentation/main/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.rentproject.presentation.rooms.RoomsViewModel
 import com.example.rentproject.ui.theme.RentProjectTheme
@@ -16,6 +17,9 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().setKeepOnScreenCondition {
+            false
+        }
         setContent {
             RentProjectTheme {
                 Surface(
---
+++ app/src/main/java/com/example/rentproject/presentation/room_details/RoomDetailsScreen.kt
@@ -0,0 +1,8 @@
+package com.example.rentproject.presentation.room_details
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RoomDetailsScreen() {
+
+}
